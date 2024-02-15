815a68ae6b33e22b4653a2754a923896b957d8eb
ayush raj <ayushraj5299@gmail.com>
2024-02-16 00:12:21 +0530

    complete

---
+++ second.js
@@ -0,0 +1,6 @@
+const a = 20 
+
+const b = -15 
+const c = 0
+const result =  a > 0 ? "a is positive" : " a is negative" 
+console.log(result)
---
+++ third.js
@@ -0,0 +1,3 @@
+const person_age = 22
+ const elegiblePerson = person_age > 18 ? " they are eligible to vote in election" : "they are not eligible to vote"
+ console.log("eligibility > 18 = ", elegiblePerson)
